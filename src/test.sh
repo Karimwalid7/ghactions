fc9cce45249c4ca69315d56bd2f24eebd85a0f91
Karim <kareemwalid19@gmail.com>
2025-08-31 16:44:02 +0300

    Creates new nodejs app

--- src/test.sh
+++ src/test.sh
@@ -2,7 +2,7 @@
 
 #src/test.sh 
 EXPECTED="HELLo, Test"
-OUTPUT=$(node -e "console.log(require('./src/app')('Test')")
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))");
 
 if [ "$OUTPUT" == "$EXPECTED" ]; then
   echo "Test passed"
